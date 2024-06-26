1964414fab7acfd04a2f96a2b405ba619dc0c4c5
darcierre <darrnurse@gmail.com>
2024-06-25 23:34:12 -0400

    Fixing git issue

---
+++ test.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def read_url():
+    response = requests.get("https://www.york.cuny.edu/++api++/admissions")
+    links = response.json()["items"]
+    links = [item['@id'] for item in links]
+    return links
+
+
+def get_event_details(event_list):
+    event_details = []
+    for event in event_list:
+        response = requests.get("https://www.york.cuny.edu/++api++/admissions")
+        response.raise_for_status()
+        event_data = response.json()
+        title = event_data["title"]
+        description = event_data["description"]
+
+        event_details.append({
+            "title": title,
+            "description": description,
+        })
+    return event_details
+
+
+admissions = read_url()
+event_details = get_event_details(admissions)
+
+print(json.dumps(event_details, indent=2))
---
+++ test2.py
@@ -0,0 +1,39 @@
+import requests
+
+def get_event_list():
+    response = requests.get("https://www.york.cuny.edu/++api++/events")
+    response.raise_for_status()
+    items = response.json()["items"]
+    event_list = [item["@id"] for item in items]
+    event_list.pop(0)  # Shift the first element
+    return event_list
+
+def insert_api_name(uri):
+    return uri.replace(".edu/", ".edu/++api++/")
+
+def get_event_details(event_list):
+    event_details = []
+    for event in event_list:
+        api_event = insert_api_name(event)
+        response = requests.get(api_event)
+        response.raise_for_status()
+        event_data = response.json()
+        title = event_data["title"]
+        description = event_data["description"]
+        start = event_data["start"]
+        end = event_data["end"]
+        location = event_data["location"]
+
+        event_details.append({
+            "title": title,
+            "description": description,
+            "start": start,
+            "end": end,
+            "location": location
+        })
+    return event_details
+
+event_list = get_event_list()
+event_details = get_event_details(event_list)
+
+print(event_details)
